3354d9d3d2877ad8a4543631b6cc67963c7fb94d
isaactobiloba <isaactobiloba7@gmail.com>
2023-05-22 16:37:57 +0100

    Modify handler class/methods to catch exceptions

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,7 +2,15 @@
 
 namespace App\Exceptions;
 
+use ErrorException;
+use GuzzleHttp\Exception\ClientException;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\Finder\Exception\AccessDeniedException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Routing\Exception\MethodNotAllowedException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -27,6 +35,15 @@ class Handler extends ExceptionHandler
         'password_confirmation',
     ];
 
+    public function report(Throwable $exception)
+    {
+        if (app()->bound('sentry') && $this->shouldReport($exception)) {
+            app('sentry')->captureException($exception);
+        }
+
+        parent::report($exception);
+    }
+
     /**
      * Register the exception handling callbacks for the application.
      *
@@ -37,5 +54,58 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    public function render($request, Throwable $exception)
+    {
+        if ($request->wantsJson()) {
+            if ($exception instanceof ModelNotFoundException || $exception instanceof NotFoundHttpException) {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Resource not found'
+                ], 404);
+            }
+
+            if (
+                $exception instanceof MethodNotAllowedException ||
+                $exception instanceof MethodNotAllowedHttpException
+            ) {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Method not allowed'
+                ], 405);
+            }
+
+            if ($exception instanceof AccessDeniedException) {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Access Denied'
+                ], 403);
+            }
+
+            if ($exception instanceof AuthenticationException) {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Unauthenticated'
+                ], 401);
+            }
+
+            if ($exception instanceof \PDOException || $exception instanceof ErrorException) {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Something is not right, please try later.'
+                ], 500);
+            }
+        }
+
+        if ($exception instanceof ClientException) {
+            return response()->json([
+                'status' => false,
+                'message' => $exception->getMessage(),
+                'data' => []
+            ], 500);
+        }
+
+        return parent::render($request, $exception);
     }
 }
